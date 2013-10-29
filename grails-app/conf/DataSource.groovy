4511042902bc7fbd9c84b046d51de189efdf47a7
Nathan Dunn <ndunn@me.com>
2013-10-29 06:44:51 -0700

    add mysql server

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,12 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
+//    driverClassName = "org.h2.Driver"
+    driverClassName = "com.mysql.jdbc.Driver"
+//    username = "sa"
     password = ""
+    username = "ubuntu"
+//    password = "Vq9NvDXS"
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -14,19 +18,23 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost:3306/speech_dev"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost:3306/speech_test"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost:3306/speech_prod"
             pooled = true
             properties {
                maxActive = -1
---
+++ scripts/initdb.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mysql --user=root mysql -e " create user 'ubuntu'@'localhost' identified by password 'Vq9NvDXS'; grant all on *.* to 'ubuntu'@'localhost' ; " 
+
+mysql -u ubuntu -pVq9NvDXS -e 'drop database if exists speech_dev ; create database speech_dev ;'
+
