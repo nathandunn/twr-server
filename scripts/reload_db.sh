73432c442fc5b0ddfce2944ccb82fcd110d99614
Nathan Dunn <ndunn@me.com>
2014-04-04 16:48:13 -0700

    udated

--- scripts/reload_db.sh
+++ scripts/reload_db.sh
@@ -1,4 +1,4 @@
 #filename=dump_`date +"%Y-%m-%d"`.sql
 #!/bin/bash
 mysql -u ubuntu -pVq9NvDXS -e 'drop database if exists speech_dev ; create database speech_dev ;'
-mysql -uubuntu -pVq9NvDXS speech_dev < dump_passage2014-04-04.sql
+mysql -uubuntu -pVq9NvDXS speech_dev < dump_2014-04-04.sql
