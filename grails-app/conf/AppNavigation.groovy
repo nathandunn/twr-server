d078067af3cc950630e79c4fc113d2d1e8d897f9
Nathan Dunn <ndunn@me.com>
2014-04-06 12:42:24 -0700

    updated nav

---
+++ grails-app/conf/AppNavigation.groovy
@@ -0,0 +1,76 @@
+//import edu.uoregon.stockdb.Researcher
+//import edu.uoregon.stockdb.ResearcherService
+//import org.apache.shiro.SecurityUtils
+
+//def loggedIn = {->
+//    return SecurityUtils.subject.authenticated
+//}
+//def loggedOut = {->
+//    return !SecurityUtils.subject.authenticated
+//}
+
+//def username = {->
+////    if(loggedIn){
+//    return ((Researcher) SecurityUtils.subject.principal).fullName
+////    }
+////    else{
+////        return "N/A"
+////    }
+//}
+
+//def getProfileName() {
+//    return ((Researcher) SecurityUtils.subject.principal).fullName
+//}
+
+//def isAdmin = {->
+//    try {
+//        SecurityUtils.subject.checkRole(ResearcherService.ROLE_ADMINISTRATOR)
+//        return true
+//    } catch (e) {
+//        println "is not admin ${SecurityUtils.subject.principal}"
+//        return false
+//    }
+//}
+
+navigation = {
+    // Declare the "app" scope, used by default in tags
+    app {
+			Transcript(controller: 'transcription', action: 'list') 
+            Passage(controller: 'passage', action: 'list')
+            ProcessingQueue(controller: 'processingQueue', action: 'list')
+
+////        // A nav item pointing to HomeController, using the default action
+//        experiment(controller: 'experiment', action: 'list') {
+//            measuredValue(controller: 'measuredValue', action: 'list')
+//            category(controller: 'category', action: 'list')
+//        }
+
+//        host(controller: 'hostOrigin', action: 'list') {
+//            genotype(controller: 'hostGenotype', action: 'list')
+//            species(controller: 'species', action: 'list')
+//            origin(controller: 'hostOrigin', action: 'list')
+//            facility(controller: 'hostFacility', action: 'list')
+//            population(controller: 'population', action: 'list')
+//        }
+
+//        labs(controller: 'lab', action: 'list') {
+//            researcher(controller: 'researcher', action: 'list')
+//        }
+
+//        auth(visible: false)
+
+    }
+
+// Some back-end admin scaffolding stuff in a separate scope
+//    admin {
+//        // Use "list" action as default item, even if its not default action
+//        // and create automatic sub-items for the other actions
+//        books(controller:'bookAdmin', action:'list, create, search')
+//
+//        // User admin, with default screen using "search" action
+//        users(controller:'userAdmin', action:'search') {
+//            // Declare action alias so "create" is active for both "create" and "update" actions
+//            create(action:'create', actionAliases:'update')
+//        }
+//    }
+}
