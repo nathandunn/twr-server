7fa5f08db9d6a18e5bc8231c5179d7b82a12a4d8
Nathan Dunn <ndunn@me.com>
2014-04-06 12:42:24 -0700

    updated nav

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -86,6 +86,7 @@ grails.project.dependency.resolution = {
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
+        compile ":platform-core:1.0.RC5"
 
 //        build ":tomcat:$grailsVersion"
 
