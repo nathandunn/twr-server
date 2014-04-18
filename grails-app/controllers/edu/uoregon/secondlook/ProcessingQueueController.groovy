f5c8625b2904ba0bed19525e698888b0f5b7b991
Nathan Dunn <ndunn@me.com>
2014-04-18 06:16:29 -0700

    updated queue

--- grails-app/controllers/edu/uoregon/secondlook/ProcessingQueueController.groovy
+++ grails-app/controllers/edu/uoregon/secondlook/ProcessingQueueController.groovy
@@ -93,10 +93,12 @@ class ProcessingQueueController {
         }
 
         try {
-            processingQueueInstance.computerTranscript.processingQueue = null
-            processingQueueInstance.computerTranscript = null
+            if(processingQueueInstance.computerTranscript){
+                processingQueueInstance.computerTranscript.processingQueue = null
+                processingQueueInstance.computerTranscript = null
 //            processingQueueInstance.transcription.processingQueues = null
 //            processingQueueInstance.computerTranscript = null
+            }
 
             processingQueueInstance.delete(flush: true)
             flash.message = message(code: 'default.deleted.message', args: [message(code: 'processingQueue.label', default: 'ProcessingQueue'), id])
