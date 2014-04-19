e85462ce90e20af04be802b937657c2953d7c3cc
Nathan Dunn <ndunn@me.com>
2014-04-19 15:03:11 -0700

    udpated researcher access

--- src/groovy/edu/uoregon/secondlook/DataStubber.groovy
+++ src/groovy/edu/uoregon/secondlook/DataStubber.groovy
@@ -1,5 +1,7 @@
 package edu.uoregon.secondlook
 
+import org.apache.shiro.crypto.hash.Sha256Hash
+
 /**
  * Created by ndunn on 4/10/14.
  */
@@ -66,5 +68,49 @@ class DataStubber {
 
 //            transcription.discard()
         }
+    }
+
+    def addAdminResearcher(String name, String email) {
+        ResearcherRole adminRole = ResearcherRole.findByName(ResearcherService.ROLE_ADMINISTRATOR)
+        addResearcher(name, email, adminRole)
+        println "Added admin researcher ${name}"
+    }
+
+    def addResearcher(String name, String email, ResearcherRole role) {
+        ResearcherUser researcher = new ResearcherUser(
+                name: name
+                , username: email
+                , passwordHash: new Sha256Hash(generatePassword()).toHex()
+        ).save(insert: true, flush: true)
+        researcher.addToRoles(role)
+        researcher.save(flush: true)
+    }
+
+    String generatePassword() {
+//        return RandomStringUtils.random(10)
+        return "testpass123"
+    }
+
+    def stubUsers() {
+        println "stubing users "
+        def adminRole = new ResearcherRole(name: ResearcherService.ROLE_ADMINISTRATOR)
+        adminRole.addToPermissions("*:*")
+        adminRole.save(failOnError: true, insert: true, flush: true)
+        def userRole = new ResearcherRole(name: ResearcherService.ROLE_USER)
+        userRole.addToPermissions("*:list")
+        userRole.addToPermissions("*:show")
+        userRole.addToPermissions("*:index")
+
+        addAdminResearcher("Steve Fickas", "fickas@cs.uoregon.edu")
+        addAdminResearcher("Jason Prideaux", "jprideau@cs.uoregon.edu")
+        addAdminResearcher("Carol Warren", "carolwarren78@gmail.com")
+        addAdminResearcher("Nathan Dunn", "ndunn@uoregon.edu")
+
+//        adminRole.addToPermissions("*:*")
+//        adminRole.save(failOnError: true, insert: true, flush: true)
+//        println "saved admin role ${ResearcherRole.count}"
+//
+//        def userRole = new ResearcherRole(name: ResearcherService.ROLE_USER)
+
     }
 }
