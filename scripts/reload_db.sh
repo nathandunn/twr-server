7438e5fec8441ac950c7e2dde7adef4f19a1c883
Nathan Dunn <ndunn@uoregon.edu>
2014-04-23 10:28:14 -0700

    updated

--- scripts/reload_db.sh
+++ scripts/reload_db.sh
@@ -1,4 +1,5 @@
 #filename=dump_`date +"%Y-%m-%d"`.sql
 #!/bin/bash
 mysql -u ubuntu -pVq9NvDXS -e 'drop database if exists speech_dev ; create database speech_dev ;'
-mysql -uubuntu -pVq9NvDXS speech_dev < dump_2014-04-17-postconvert.sql
+#mysql -uubuntu -pVq9NvDXS speech_dev < dump_2014-04-17-postconvert.sql
+mysql -uubuntu -pVq9NvDXS speech_dev < dump_2014-04-23.sql
