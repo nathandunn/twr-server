815e735ff38f8e50a4cd9847b6eff63d9d51c465
Nathan Dunn <ndunn@me.com>
2014-05-05 10:38:17 -0700

    udpated reload

--- scripts/reload_db.sh
+++ scripts/reload_db.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 mysql -u ubuntu -pVq9NvDXS -e 'drop database if exists speech_dev ; create database speech_dev ;'
 #mysql -uubuntu -pVq9NvDXS speech_dev < dump_2014-04-17-postconvert.sql
-mysql -uubuntu -pVq9NvDXS speech_dev < dump_2014-04-23.sql
+mysql -uubuntu -pVq9NvDXS speech_dev < dump_2014-05-05.sql
